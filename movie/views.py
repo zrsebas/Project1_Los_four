49c656488d0d390002b5ec612ca3f7e25d6c920e
zrsebas <sebastianzare10@gmail.com>
2026-08-02 16:11:08 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
-    #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Sebastian Zapata'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
     return render(request, 'about.html')
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home),
     path('about/', movieViews.about),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
